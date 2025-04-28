8d0f04b33b4591509bc67877a1360e702d56db8d
OLeviAlves <lelealves529@gmail.com>
2025-04-28 19:34:55 -0300

    Configuração do DBO e da tabela Veiculos

---
+++ Mf_Dev_BackEnd/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mf_Dev_BackEnd.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ Mf_Dev_BackEnd/Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mf_Dev_BackEnd.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required(ErrorMessage ="Obrigatorio informar um nome!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar uma placa!")]
+        public int Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o ano de frabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o ano do modelo!")]
+        public int AnoModelo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar a cor do carro!")]
+        public string Cor { get; set; } 
+    }
+}
